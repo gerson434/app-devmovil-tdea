8ee6e73228dd6005b883f979db43331f664a646e
dahianarestrepo23 <yakeposada23@gmail.com>
2023-04-12 22:44:18 -0500

    parte de yake

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  posts:any; 
+  
+
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private firestore : AngularFirestore,
+    private navCtrl: NavController,
+    ) { }
+
+  ngOnInit() {
+  }
+ionViewWillEnter(){
+  this.getPosts();
+}
+async Nav(id: string) {
+  this.navCtrl.navigateRoot("edit-page/edit-page");
+}
+async getPosts(){
+  let loader = await this.loadingCtrl.create({
+    message: "Espere un momento por favor..."
+  });
+  await loader.present();
+
+  try {
+    this.firestore.collection('posts')
+      .snapshotChanges()
+      .subscribe((data:any[]) =>{
+
+        this.posts = data.map((e:any) =>{
+          return{
+            id: e.payload.doc.id,
+            title: e.payload.doc.data()["title"],
+            details: e.payload.doc.data()["details"]
+        }
+      });
+    }); 
+    
+    await loader.dismiss();
+  } catch (e:any) {
+    e.message = "mensaje de error del home";
+    let errorMessage = e.message || e.getLocalizedMessage();
+      
+      this.showToast(errorMessage); 
+  }
+}
+
+async deletePost(id: string){    
+  let loader = await this.loadingCtrl.create({
+    message: "Espere un momento por favor..."
+  });
+  await loader.present();
+
+  await this.firestore.doc("posts/"+id).delete();
+
+  await loader.dismiss();  
+}
+
+showToast(message: string) {
+  this.toastCtrl.create({
+    message: message,
+    duration: 5000 // Aumenta la duración del mensaje a 5 segundos
+  }).then(toastData => toastData.present());
+}
+}
